9b1f09bdfa4200a196cdde5cdf9930e7fdc5e0b9
Hugo Torres <noreply@redactado.es>
2025-05-17 19:54:10 +0200

    Add Listeners class for managing event listeners in the application

---
+++ src/main/java/es/redactado/config/Listeners.java
@@ -0,0 +1,12 @@
+package es.redactado.config;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.lang.invoke.CallSite;
+import java.util.List;
+
+public class Listeners {
+    public static final List<Class<? extends ListenerAdapter>> LISTENERS = List.of(
+            // Add your listeners here
+    );
+}
